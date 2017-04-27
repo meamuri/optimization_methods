68639194070da4ec81e85ce37372361a5852a7eb
meamuri <meammf@yandex.ru>
2017-04-27 12:31:02 +0300

    newton method is ready

--- src/methods/newton.rs
+++ src/methods/newton.rs
@@ -1,4 +1,5 @@
 use std::f64;
+use std::num;
 
 fn first_derivative(f: &Fn(f64)-> f64, x: f64, h: f64) -> f64 {
     return (f(x + h) - f(x)) / h;
@@ -6,4 +7,17 @@ fn first_derivative(f: &Fn(f64)-> f64, x: f64, h: f64) -> f64 {
 
 fn second_derivative(f: &Fn(f64) -> f64, x: f64, h: f64) -> f64 {
     return (f(x + h) - 2.0*f(x) + f(x - h)) / h.powf(2.0);
+}
+
+pub fn newton_method(a: f64, b: f64, _u0: f64, eps: f64, 
+    f: &Fn(f64) -> f64) -> f64 {
+    let mut u0 = _u0;
+    while first_derivative(f, u0, eps).abs() >= eps &&
+        u0 >= a && u0 <= b{
+            u0 -= first_derivative(f, u0, eps) / second_derivative(f, u0, eps);
+        }
+    if (u0 >= a && u0 <= b){
+        return u0;
+    }
+    return if u0 <= a { a } else { b };
 }
