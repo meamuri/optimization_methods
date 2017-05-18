91ff7f1a474a59bde8ce0cbce2438c25f029a386
meamuri <meammf@yandex.ru>
2017-05-18 11:30:33 +0300

    infty loops were added

--- src/methods/multi_dim_methods/fastest_descent.rs
+++ src/methods/multi_dim_methods/fastest_descent.rs
@@ -4,14 +4,17 @@ fn segment_div_help(x: f64, y: f64) -> f64 {
 
 pub fn fastest_descent(x: f64, y: f64, eps: f64, f: &Fn(f64, f64) -> f64) -> (f64, f64) {
     let (mut x, mut y) = (x, y);
-    let mut grad_x = super::x_derivative(x, y, eps, f);
-    let mut grad_y = super::y_derivative(x, y, eps, f);
-    while super::norm(grad_x, grad_y) >= eps {
+
+    loop {
+        let grad_x = super::x_derivative(x, y, eps, f);
+        let grad_y = super::y_derivative(x, y, eps, f);    
+        if super::norm(grad_x, grad_y) < eps{
+            break;
+        }
         let alpha_new = segment_div_help(x, y);
         x -= alpha_new * grad_x;
-        y -= alpha_new * grad_y;
-        grad_x = super::x_derivative(x, y, eps, f);
-        grad_y = super::y_derivative(x, y, eps, f);
+        y -= alpha_new * grad_y;        
     }
+    
     (x, y)
 }
--- src/methods/multi_dim_methods/splitting_of_step.rs
+++ src/methods/multi_dim_methods/splitting_of_step.rs
@@ -1,9 +1,15 @@
 pub fn splitting_of_step(x: f64, y: f64, alpha: f64, eps: f64, f: &Fn(f64, f64) -> f64) -> (f64, f64) {
     let (mut x, mut y, mut alpha) = (x, y, alpha);
     let mut i0 = f(x, y);
-    let mut grad_x = super::x_derivative(x, y, eps, f);
-    let mut grad_y = super::y_derivative(x, y, eps, f);
-    while super::norm(grad_x, grad_y) >= eps {
+    
+    loop {
+        let grad_x = super::x_derivative(x, y, eps, f);
+        let grad_y = super::y_derivative(x, y, eps, f);
+
+        if super::norm(grad_x, grad_y) < eps{
+            break;
+        }
+
         let x1 = x - alpha * grad_x;
         let y1 = y - alpha * grad_y;
         let i1 = f(x1, y1);
@@ -14,8 +20,6 @@ pub fn splitting_of_step(x: f64, y: f64, alpha: f64, eps: f64, f: &Fn(f64, f64) 
             y = y1;
             i0 = i1;
         }
-        grad_x = super::x_derivative(x, y, eps, f);
-        grad_y = super::y_derivative(x, y, eps, f);
     }
     (x, y)
 }
